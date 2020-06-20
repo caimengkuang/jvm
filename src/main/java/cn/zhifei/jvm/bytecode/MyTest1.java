98de9eea9979a52ebe5f0cbeb7599957720bfda0
yase <cmk@goujiawang.com>
2020-06-20 18:54:14 +0800

    #feature

--- src/main/java/cn/zhifei/jvm/bytecode/MyTest1.java
+++ src/main/java/cn/zhifei/jvm/bytecode/MyTest1.java
@@ -1,4 +1,17 @@
 package cn.zhifei.jvm.bytecode;
 
 public class MyTest1 {
+
+    private int a = 1;
+
+    public MyTest1() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
 }
